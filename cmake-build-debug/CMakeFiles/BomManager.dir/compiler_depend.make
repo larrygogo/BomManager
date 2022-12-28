f1376315c77a56e729bb5bef449faee483365517
Larry <351220018@qq.com>
2022-12-28 16:20:23 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/BomManager.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BomManager.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/BomManager.dir/lib/file.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/BomManager.dir/lib/file.c.o: \
+  /Users/larry/CLionProjects/BomManager/lib/file.c \
+  /Users/larry/CLionProjects/BomManager/lib/file.h
