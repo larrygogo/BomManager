63cea78577e3fb19415d42e860adf9e25eee801e
Larry <351220018@qq.com>
2023-02-21 09:40:03 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/BomManager.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/larry/CLionProjects/BomManager/lib/formula.c" "CMakeFiles/BomManager.dir/lib/formula.c.o" "gcc" "CMakeFiles/BomManager.dir/lib/formula.c.o.d"
+  "/Users/larry/CLionProjects/BomManager/lib/menu.c" "CMakeFiles/BomManager.dir/lib/menu.c.o" "gcc" "CMakeFiles/BomManager.dir/lib/menu.c.o.d"
+  "/Users/larry/CLionProjects/BomManager/lib/part.c" "CMakeFiles/BomManager.dir/lib/part.c.o" "gcc" "CMakeFiles/BomManager.dir/lib/part.c.o.d"
+  "/Users/larry/CLionProjects/BomManager/lib/partCount.c" "CMakeFiles/BomManager.dir/lib/partCount.c.o" "gcc" "CMakeFiles/BomManager.dir/lib/partCount.c.o.d"
+  "/Users/larry/CLionProjects/BomManager/lib/utils.c" "CMakeFiles/BomManager.dir/lib/utils.c.o" "gcc" "CMakeFiles/BomManager.dir/lib/utils.c.o.d"
+  "/Users/larry/CLionProjects/BomManager/main.c" "CMakeFiles/BomManager.dir/main.c.o" "gcc" "CMakeFiles/BomManager.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BomManager.dir/main.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/BomManager.dir/main.c.o: \
+  /Users/larry/CLionProjects/BomManager/main.c \
+  /Users/larry/CLionProjects/BomManager/lib/types.h \
+  /Users/larry/CLionProjects/BomManager/lib/menu.h \
+  /Users/larry/CLionProjects/BomManager/lib/utils.h
